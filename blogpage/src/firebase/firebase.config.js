baa0a803b56f4c956de01d64104750539ba6706f
Paulo Alexandre <paulequilibrio@gmail.com>
2020-02-13 07:47:22 -0300

    Add firebase config

---
+++ blogpage/src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+export default {
+	apiKey: 'AIzaSyCctB4ALRFJE-NOepps0yeHiroJte_5qIQ',
+	authDomain: 'ztm-community-blog-posting.firebaseapp.com',
+	databaseURL: 'https://ztm-community-blog-posting.firebaseio.com',
+	projectId: 'ztm-community-blog-posting',
+	storageBucket: 'ztm-community-blog-posting.appspot.com',
+	messagingSenderId: '545147993667',
+	appId: '1:545147993667:web:c1940ef8f5b14cead99ef6',
+	measurementId: 'G-GL2XXE02JP',
+};
